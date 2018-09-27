9108d58d8176f4bb2361ea9c1daa410dc9e21d45
Huan L <huanl@Huans-MacBook-Pro.local>
2018-09-26 18:45:44 -0700

    Re-factored server's index.js, to make it flexible to different DBs

--- database/index.js
+++ database/index.js
@@ -1,15 +1,22 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://david:password1234@ds111113.mlab.com:11113/spotify-header');
+const mongoose = require("mongoose");
+// [Todo] Create my own account on mlab.com
+// mongoose.connect('mongodb://david:password1234@ds111113.mlab.com:11113/spotify-header');
+mongoose.connect(
+  // "mongodb://localhost/dotThen",
+  "mongodb://localhost/skyBeat",
+  { useNewUrlParser: true }
+);
 
 const db = mongoose.connection;
-db.on('error', error => {
+db.on("error", error => {
   console.error(error);
 });
-db.once('open', () => {
-  console.log('MONGOOSE CONNECTED!');
+db.once("open", () => {
+  console.log("MONGOOSE CONNECTED!");
 });
 
-const headerDBSchema = new mongoose.Schema({
+// const headerDBSchema = new mongoose.Schema({
+const artistSchema = new mongoose.Schema({
   artistID: {
     type: Number,
     unique: true
@@ -24,8 +31,22 @@ const headerDBSchema = new mongoose.Schema({
   }
 });
 
-const HeaderDB = mongoose.model('HeaderDB', headerDBSchema);
-module.exports = HeaderDB;
+// const HeaderDB = mongoose.model("HeaderDB", headerDBSchema);
+const Artists = mongoose.model("Artists", artistSchema);
+
+// Newly refactored
+const getArtist = function(artistID, callback) {
+  Artists.find({ artistID: artistID }, function(err, artistObj) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, artistObj);
+    }
+  });
+};
+
+// module.exports.Artists = Artists;
+module.exports.getArtist = getArtist;
 
 // NOTE: To be used later
 // const userSchema = new mongoose.Schema({
--- database/seeder.js
+++ database/seeder.js
@@ -1,38 +1,51 @@
-const faker = require('faker');
-let HeaderDB = require('./index.js');
-faker.locale = 'en_US';
+const faker = require("faker");
+const Artists = require("./index.js");
+faker.locale = "en_US";
 
 for (let i = 1; i <= 100; i++) {
   let newArtist = {
     artistID: i,
     artistName: faker.name.findName(),
     followed: Math.round(Math.random()) ? true : false,
-    followersNumber: 'PLACE_HOLDER',
+    followersNumber: "PLACE_HOLDER",
     verified: Math.round(Math.random() * 0.7 + 0.3) ? true : false,
     artistImages: [],
     about: {
-      Biography: faker.lorem.paragraphs(faker.random.number({ min: 5, max: 10 }), '\n\n'),
+      Biography: faker.lorem.paragraphs(
+        faker.random.number({ min: 5, max: 10 }),
+        "\n\n"
+      ),
       Where: {}
     }
   };
   var a = faker.random.number();
   // adding cities
   for (let i = 0; i < faker.random.number({ min: 80, max: 200 }); i++) {
-    newArtist.about.Where[faker.address.city()] = faker.random.number({ min: 1000, max: 100000 });
+    newArtist.about.Where[faker.address.city()] = faker.random.number({
+      min: 1000,
+      max: 100000
+    });
   }
 
   // adding urls to s3;
 
   var obj = {};
   for (let i = 1; i <= faker.random.number({ min: 2, max: 10 }); i++) {
-    obj[faker.random.number({ min: 1, max: 27 })] = faker.random.number({ min: 1, max: 27 });
+    obj[faker.random.number({ min: 1, max: 27 })] = faker.random.number({
+      min: 1,
+      max: 27
+    });
   }
   Object.values(obj).forEach(num => {
-    newArtist.artistImages.push(`https://s3-us-west-1.amazonaws.com/spotifycloneartists/photos/${num}.jpg`);
+    newArtist.artistImages.push(
+      `https://s3-us-west-1.amazonaws.com/spotifycloneartists/photos/${num}.jpg`
+    );
   });
 
-  newArtist.followersNumber = Object.values(newArtist.about.Where).reduce((accumulator, currentValue) => accumulator + currentValue);
-  HeaderDB.create(newArtist, function(err, newData) {
+  newArtist.followersNumber = Object.values(newArtist.about.Where).reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+  Artists.create(newArtist, function(err, newData) {
     err ? console.error(error) : console.log(newData);
   });
 }
