0fba0f226ddcc8fba73cb7f076cb5bb90a801c05
Lancy L <lancylh@gmail.com>
2018-09-28 13:01:38 -0700

    Updated seeder.js for data gen (#2)
    * server testing ready for review. front-end testing needs work
    
    * comming changes before moving on
    
    * oops
    
    * play and pause buttons complete
    
    * play and follow button complete
    
    * added jestOutput to .gitignore
    
    * removed jestOutput from git
    
    * minor stylistic changes
    
    * ...Button ready for review
    
    * added comments
    
    * made variable naming consistent across files
    
    * created About.jsx component
    
    * nav bar component setup
    
    * got icons
    
    * cleaned up the nav bar a bit
    
    * location stat complete
    
    * biography showing properly
    
    * switched about section to grid
    
    * styling done for about section. going to work on body collapsible
    
    * swapped out the inlinesvg package
    
    * got the dropdown to show above carousel. going to work on bio collapse
    
    * updated readme.md
    
    * added window.HeaderApp = HeaderApp;
    
    * readme fixed
    
    * partially resolved carousel blocking dropdown
    
    * read me fix
    
    * moved index file out of gitignore
    
    * uploading bundle.js
    
    * added cors
    
    * Bar icons fixed, images stored are higher quality
    
    * changed webpack bundle to watch
    
    * carousel no longer hidden
    
    * about to integrate rc-dropdown
    
    * about to implement rc-drop
    
    * build script added
    
    * seeder updated to redirect to s3
    
    * switched content order
    
    * nav bar and expanding transition working
    
    * production bundle produced
    
    * database connected to mlab
    
    * webpack 4 upgrade
    
    * proxy server compressed
    
    * Re-factored server's index.js, to make it flexible to different DBs
    
    * Added 4 server requests (CRUD) to server's index.js
    
    * Added write and read functions to seeder.js; can generate 100K records to DB table now
    
    * Updated seeder.js for data gen
    
    * Updated seeder.js, for data gen

--- database/seeder.js
+++ database/seeder.js
@@ -1,52 +1,143 @@
+// Note: Biography and Where fields do not need to start with upper case, though I won't change them (due to high effort, low reward)
+
+// For both steps below: write and read with local path
+// Note: stream also works for non-objects
+const jsonfile = require("jsonfile");
+const fs = require("fs");
+
+// Step 1: generate data, then load it to local storage file
+const stream = fs.createWriteStream("./database/artists.json");
+const maxChunk = 10000; // Set to be 25K (or 50K), later
+const rounds = 10;
+
+// 1(a): generate data
 const faker = require("faker");
-const Artists = require("./index.js");
 faker.locale = "en_US";
 
-for (let i = 1; i <= 100; i++) {
-  let newArtist = {
-    artistID: i,
-    artistName: faker.name.findName(),
-    followed: Math.round(Math.random()) ? true : false,
-    followersNumber: "PLACE_HOLDER",
-    verified: Math.round(Math.random() * 0.7 + 0.3) ? true : false,
-    artistImages: [],
-    about: {
-      Biography: faker.lorem.paragraphs(
-        faker.random.number({ min: 5, max: 10 }),
-        "\n\n"
-      ),
-      Where: {}
+function generateArtists(mc, currentRound) {
+  let artistsArr = [];
+  let startingIndex = 1 + (currentRound - 1) * mc;
+  for (let i = startingIndex; i <= startingIndex + mc - 1; i++) {
+    let newArtist = {
+      artistID: i,
+      artistName: faker.name.findName(),
+      followed: Math.round(Math.random()) ? true : false,
+      followersNumber: "PLACE_HOLDER",
+      verified: Math.round(Math.random() * 0.7 + 0.3) ? true : false,
+      artistImages: [],
+      about: {
+        Biography: faker.lorem.paragraphs(
+          // Newly modified: reduced the number of paragraphs, for DB performance
+          faker.random.number({ min: 3, max: 6 }),
+          "\n\n"
+        ),
+        Where: {}
+      }
+    };
+    // Where: adding cities and number of people
+    // Newly modified: reduced the number of cities per artist, for DB performance
+    for (let i = 0; i < faker.random.number({ min: 5, max: 15 }); i++) {
+      newArtist.about.Where[faker.address.city()] = faker.random.number({
+        min: 1000,
+        max: 100000
+      });
     }
-  };
-  var a = faker.random.number();
-  // adding cities
-  for (let i = 0; i < faker.random.number({ min: 8, max: 20 }); i++) {
-    // for (let i = 0; i < faker.random.number({ min: 80, max: 200 }); i++) {
-    newArtist.about.Where[faker.address.city()] = faker.random.number({
-      min: 1000,
-      max: 100000
+    // Note: per artist, key is number of photos, value is photo number for S3's url
+    var obj = {};
+    // Newly modified: reduced the number of photos per artist, for DB performance
+    for (let i = 1; i <= faker.random.number({ min: 2, max: 5 }); i++) {
+      obj[faker.random.number({ min: 1, max: 27 })] = faker.random.number({
+        min: 1,
+        max: 27
+      });
+    }
+    Object.values(obj).forEach(num => {
+      newArtist.artistImages.push(
+        `https://s3-us-west-1.amazonaws.com/spotifycloneartists/photos/${num}.jpg` // S3 urls
+      );
     });
-  }
-
-  // adding urls to s3;
-
-  var obj = {};
-  for (let i = 1; i <= faker.random.number({ min: 2, max: 10 }); i++) {
-    obj[faker.random.number({ min: 1, max: 27 })] = faker.random.number({
-      min: 1,
-      max: 27
-    });
-  }
-  Object.values(obj).forEach(num => {
-    newArtist.artistImages.push(
-      `https://s3-us-west-1.amazonaws.com/spotifycloneartists/photos/${num}.jpg`
+    // Get number of followers
+    newArtist.followersNumber = Object.values(newArtist.about.Where).reduce(
+      (accumulator, currentValue) => accumulator + currentValue
     );
-  });
+    // console.log(JSON.stringify(newArtist)); // For testing
+    artistsArr.push(newArtist);
+  }
+  return JSON.stringify(artistsArr); // Newly modified
+}
 
-  newArtist.followersNumber = Object.values(newArtist.about.Where).reduce(
-    (accumulator, currentValue) => accumulator + currentValue
-  );
-  Artists.create(newArtist, function(err, newData) {
-    err ? console.error(error) : console.log(newData);
+// 1(b): append data to local file
+stream.write('{"artists":[');
+for (let j = 1; j <= rounds; j++) {
+  console.log("Round number " + j);
+  let newString = generateArtists(maxChunk, j);
+  if (j < rounds) {
+    stream.write(newString.substring(1, newString.length - 1) + ",");
+  } else {
+    stream.write(newString.substring(1, newString.length - 1));
+  }
+}
+stream.write("]}");
+
+
+// Step 2: load data from local file to MongoDB's table
+const readStream = fs.createReadStream("./database/artists.json");
+const controller = require("./index.js"); // Importing DB's controller, for step 2(b)
+
+// 2(a): Load data from local file
+// Note: need to invoke step 2(b) in 2(a), because obj only exists in 2(a)'s context (object is copy by reference)
+
+// for (let k = 0; k < 10; k++) {
+jsonfile
+  .readFile("./database/artists.json")
+  // , function(err, obj) {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log("Read complete");
+  //     console.log("Type of obj " + typeof obj.artists);
+  //     insertArtists(obj.artists);
+  //   }
+  // });
+  .then(obj => {
+    console.log("Read complete");
+    insertArtists(obj.artists[0]);
+    // console.log("obj.artists is array: " + Array.isArray(obj.artists)); // Type is array
+  })
+  .catch(error => console.log(error));
+// }
+
+// 2(b): Insert data to DB table
+// Note: no need to use Promise here, Promise is just a wrapper for callbacks
+function insertArtists(artistsFromFile) {
+  controller.Artists.insertMany(artistsFromFile, function(err) {
+    if (err) {
+      console.log(err);
+    }
   });
 }
+
+/*
+  Previous/old scripts
+*/
+
+// Script used for option 2's step 1(b)
+// for (let j = 0; j < 10; j++) {
+//   console.log("Chunk number " + j);
+//   jsonfile
+//     .writeFile(
+//       "./database/artists.json",
+//       generateArtists(10),
+//       { flag: "a" }
+//     )
+//     .then(res => {
+//       console.log("Write complete");
+//     })
+//     .catch(error => console.log(error));
+// }
+
+// Original option: for inserting 100 records
+// controller.Artists.create(newArtist, function(err, newData) {
+//   err ? console.error(error) : console.log(newData);
+// });
+// }
